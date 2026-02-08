33a4e51fb91f0e02f4611a7561ba93e1f9871457
Minhaz <mminhazurrahman0@gmail.com>
2026-02-08 17:01:47 +0600

    Services design

--- components/sections/CTA.tsx
+++ components/sections/CTA.tsx
@@ -1,7 +1,7 @@
 export default function CTA() {
   return (
-      <section id="cta" className="min-h-screen flex items-center justify-center border-b border-neutral-200">
-        <h2>Featured work</h2>
+      <section id="cta" className="relative z-20 min-h-screen flex items-center justify-center border-b border-neutral-200 bg-white">
+          <h2>CTA</h2>
       </section>
   );
 }
--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer id="footer" className="flex-1 flex items-center justify-center">
-      <h2>Footer</h2>
-    </footer>
+      <section id="footer" className="relative z-20 min-h-screen flex items-center justify-center border-b border-neutral-200 bg-white">
+          <h2>Footer</h2>
+      </section>
   );
 }
