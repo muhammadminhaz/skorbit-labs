b18850346f5008b2365fde0121580b62712f7e69
Minhaz <mminhazurrahman0@gmail.com>
2026-02-12 09:30:28 +0600

    footer

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -146,7 +146,7 @@ export default function Testimonials() {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {testimonials.map((testimonial, index) => (
-                        <div key={index} ref={el => cardsRef.current[index] = el}>
+                        <div key={index} ref={el => { cardsRef.current[index] = el; }}>
                             <TestimonialCard testimonial={testimonial} />
                         </div>
                     ))}
